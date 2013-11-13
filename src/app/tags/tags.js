e25b351baf9435b36f8bc63d3015cea7cb8b006c
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2013-11-13 14:15:21 +0100

    Adding suport for tags via bootstrap-tagsinput

--- src/app/tags/tags.js
+++ src/app/tags/tags.js
@@ -1,24 +1,23 @@
 define(['backbone_p', 'jquery', 'lodash'], function(Backbone, $, _) {
-    // return Backbone.View.extend({
-    //     el: '<span><input type="text" value="eins,zwei" class="form-control" /></span>',
-    //     initialize: function() {
-    //         this.$input = this.$('input');
-    //         this.$input.tagsinput();
-    //         this.$input.tagsinput('input').typeahead({
-    //             local: ['Amsterdam', 'Washington', 'Sydney', 'Beijing', 'Cairo']
-    //         }).on('typeahead:selected', _.bind(function(obj, data) {
-    //             this.$input.tagsinput('add', data.value);
-    //             this.$input.tagsinput('input').typeahead('setQuery', '');
-    //         }, this));
-    //     }
-    // });
-
     return Backbone.View.extend({
-        el: '<div class="form-group"><input class="form-control input-sm" multiple="1" /></div>',
+        el: '<div class="form-group"><input type="text" value="eins,zwei" class="form-control" /></div>',
         initialize: function() {
-            this.$('.form-control').select2({
-                tags: ['Amsterdam', 'Washington', 'Sydney', 'Beijing', 'Cairo'],
-                tokenSeparators: [',', ' ']
+            this.$input = this.$('input');
+            this.$input.tagsinput({
+                confirmKeys: [$.KeyEvent.DOM_VK_RETURN, $.KeyEvent.DOM_VK_TAB]
+            });
+
+            // add typeahead support
+            this.$input.tagsinput('input').typeahead({
+                local: ['Amsterdam', 'Washington', 'Sydney', 'Beijing', 'Cairo']
+            }).on('typeahead:selected', _.bind(function(obj, data) {
+                this.$input.tagsinput('add', data.value);
+                this.$input.tagsinput('input').typeahead('setQuery', '');
+            }, this));
+
+            // fix typeahead to forget queries that didn't make it into tags 
+            this.$('.tt-query').on('blur', function(e) {
+                e.target.value = '';
             });
         }
     });
